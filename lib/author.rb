b073ec046003234998166c9fb691a5b5ef8e0037
Alehandro Persaud <andrew041994@gmail.com>
2020-02-04 20:16:21 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name
+  @@post_count = 0
   def initialize(name)
     @name = name
     @posts = []
@@ -12,11 +13,18 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(name)
     @posts << post = Post.new(name)
     post.author = self
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count
+    
   end
 
 
